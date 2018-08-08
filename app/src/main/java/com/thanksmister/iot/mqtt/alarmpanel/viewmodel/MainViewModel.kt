71836093b191bbb7b00fba6b34ebc97f78482a33
Michael Ritchie <mister@thanksmister.com>
2018-08-08 14:50:52 -0300

    * Fix for reversed home and away delay times

--- app/src/main/java/com/thanksmister/iot/mqtt/alarmpanel/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/thanksmister/iot/mqtt/alarmpanel/viewmodel/MainViewModel.kt
@@ -100,9 +100,9 @@ constructor(application: Application, private val messageDataSource: MessageDao,
     }
 
     fun getAlarmDelayTime(): Int {
-        if(getAlarmMode() == MODE_ARM_AWAY || getAlarmMode() == MODE_HOME_TRIGGERED_PENDING) {
+        if(getAlarmMode() == MODE_ARM_AWAY || getAlarmMode() == MODE_AWAY_TRIGGERED_PENDING) {
             return configuration.delayAwayTime
-        } else if (getAlarmMode() == MODE_ARM_HOME || getAlarmMode() == MODE_AWAY_TRIGGERED_PENDING) {
+        } else if (getAlarmMode() == MODE_ARM_HOME || getAlarmMode() == MODE_HOME_TRIGGERED_PENDING) {
             return configuration.delayHomeTime
         }
         return configuration.delayTime
